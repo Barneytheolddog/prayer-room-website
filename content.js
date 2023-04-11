f9d8e4c89fd1d10b2ba952f42b5d80820da878bf
Barneytheolddog <130314604+Barneytheolddog@users.noreply.github.com>
2023-04-11 22:13:11 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
-
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    // code to run if user is on a mobile device
+    var element = document.querySelector('.mobile-hide');
+    element.style.display = 'none';
+} else {
+    // code to run if user is on a desktop device
+}
