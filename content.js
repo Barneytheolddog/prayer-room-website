78b59c5e397d72ef1367ca32c3b3fca36d6fc529
Barneytheolddog <130314604+Barneytheolddog@users.noreply.github.com>
2023-04-12 15:20:31 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,13 +1,12 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-  // Alert message to be displayed
-  function mobileAlert() {
-    alert('This website is not compatible on mobile devices');
+  function myFunction() {
+    // Code to be executed
+    alert('This website is not compatible on mobile devices')
   }
 
   // Call the function every 1 second using setInterval()
-  setInterval(mobileAlert, 1000);
+  setInterval(myFunction, 1000);
 
-  // Hide the element with class "mobile-hide"
   var element = document.querySelector('.mobile-hide');
   element.style.display = 'none';
 } else {
